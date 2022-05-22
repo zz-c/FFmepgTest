143c62552085533039d348e822bbccf9dfd4912c
zz <35785077@qq.com>
2022-05-22 13:07:34 +0800

    rtsp

---
+++ RtspTest/main.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+extern "C" {
+#include "libavcodec/avcodec.h"
+}
+
+int main()
+{
+    std::cout << "Test Rtsp" << std::endl;
+    std::cout << avcodec_configuration() << std::endl;
+    getchar();
+}
